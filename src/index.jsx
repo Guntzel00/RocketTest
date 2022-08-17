0c172c21c6f90815b7e9f973716e1e98e7f52c2c
Gustavo Guntzel <gustavo.g.s01@hotmail.com>
2022-08-17 13:51:36 -0300

    Adding basic structre and resolving api call

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+// render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab='home' />);
